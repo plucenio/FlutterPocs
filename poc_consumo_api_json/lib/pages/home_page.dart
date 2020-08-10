71440413e50fbb0c3ddc468ea343ae1f64ec2da1
Filipe Plucenio <plucenio@gmail.com>
2020-08-10 11:12:02 -0300

    Trabalhando com Futures. Provas de conceitos.

--- poc_consumo_api_json/lib/pages/home_page.dart
+++ poc_consumo_api_json/lib/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:poc_consumo_api_json/models/hospital.dart';
-import 'package:poc_consumo_api_json/models/hospital_list.dart';
 import 'package:poc_consumo_api_json/services/api_service.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,44 +12,46 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Future<HospitalList> hospitals;
   ApiService _apiService;
 
   @override
   void initState() {
     _apiService = ApiService();
-    hospitals = _apiService.fetchPost();
     super.initState();
   }
+
+  Future<List<Hospital>> getHospitalNames() async =>
+      await _apiService.fetchPost().then((value) => value.hospitals);
+
+  Future<String> getWidget() =>
+      getHospitalNames().then((value) => value.first.name);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Obtendo dados da Web - Exemplo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Pegando dados da Web'),
+        title: 'Obtendo dados da Web - Exemplo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-        body: Center(
-          child: FutureBuilder<Hospital>(
-            future: hospitals.then((value) => value.hospitals.first),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('Id :' + snapshot.data.name.toString()));
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
-              // By default, show a loading spinner
-              return CircularProgressIndicator();
-            },
-          ),
-        ),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Pegando dados da Web'),
+            ),
+            body: FutureBuilder(
+                future: getWidget(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Center(
+                      child: Text(
+                        snapshot.data,
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                    );
+                  } else {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                })));
   }
 }
