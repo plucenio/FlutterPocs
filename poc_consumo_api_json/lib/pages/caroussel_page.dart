e38414df1c333bfc8e8eed6dc92beb7a8cf0805d
Filipe Plucenio <plucenio@gmail.com>
2020-08-13 11:03:27 -0300

    Added dots indicator,                 enableInfiniteScroll: false

--- poc_consumo_api_json/lib/pages/caroussel_page.dart
+++ poc_consumo_api_json/lib/pages/caroussel_page.dart
@@ -20,13 +20,15 @@ class CarousselPage extends StatefulWidget {
 }
 
 class _CarousselPageState extends State<CarousselPage> {
+  int _current = 0;
+
   @override
   Widget build(BuildContext context) {
     return DefaultScaffold(
       titleWidget: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(Icons.home),
+          Icon(Icons.swap_horiz),
           SizedBox(
             width: 5,
           ),
@@ -37,12 +39,35 @@ class _CarousselPageState extends State<CarousselPage> {
           child: Column(
         children: <Widget>[
           CarouselSlider(
-            options: CarouselOptions(
-              autoPlay: true,
-              aspectRatio: 2.0,
-              enlargeCenterPage: true,
-            ),
             items: imageSliders,
+            options: CarouselOptions(
+                autoPlay: true,
+                pauseAutoPlayInFiniteScroll: true,
+                enlargeCenterPage: true,
+                aspectRatio: 2.0,
+                enableInfiniteScroll: false,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                }),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: imgList.map((url) {
+              int index = imgList.indexOf(url);
+              return Container(
+                width: 8.0,
+                height: 8.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _current == index
+                      ? Color.fromRGBO(0, 0, 0, 0.9)
+                      : Color.fromRGBO(0, 0, 0, 0.4),
+                ),
+              );
+            }).toList(),
           ),
         ],
       )),
@@ -51,43 +76,66 @@ class _CarousselPageState extends State<CarousselPage> {
 }
 
 final List<Widget> imageSliders = imgList
-    .map((item) => Container(
-          child: Container(
-            margin: EdgeInsets.all(5.0),
-            child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                child: Stack(
-                  children: <Widget>[
-                    Image.network(item, fit: BoxFit.cover, width: 1000.0),
-                    Positioned(
-                      bottom: 0.0,
-                      left: 0.0,
-                      right: 0.0,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              Color.fromARGB(200, 0, 0, 0),
-                              Color.fromARGB(0, 0, 0, 0)
-                            ],
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter,
-                          ),
-                        ),
-                        padding: EdgeInsets.symmetric(
-                            vertical: 10.0, horizontal: 20.0),
-                        child: Text(
-                          'Imagem número ${imgList.indexOf(item)}',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
+    .map(
+      (item) => Container(
+        child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: pacificBlueColor),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  color: Colors.transparent,
+                ),
+                child: Column(
+                  children: [
+                    Image.asset(
+                      item,
+                      fit: BoxFit.scaleDown,
                     ),
+                    Expanded(child: Text("Imagem")),
+                    Expanded(child: Text("Descrição da imagem"))
                   ],
-                )),
-          ),
-        ))
+                ),
+              )
+
+              /*Stack(
+            children: <Widget>[
+              Image.network(item, fit: BoxFit.cover, width: 1000.0),
+              Positioned(
+                bottom: 0.0,
+                left: 0.0,
+                right: 0.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromARGB(200, 0, 0, 0),
+                        Color.fromARGB(0, 0, 0, 0)
+                      ],
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                    ),
+                  ),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  child: Text(
+                    'Imagem número ${imgList.indexOf(item)}',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),*/
+              ),
+        ),
+      ),
+    )
     .toList();
